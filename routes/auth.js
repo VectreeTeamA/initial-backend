91cc3c5728fba121e0037682ba20f742d43dc766
telpenair <telpenair@gmail.com>
2019-01-02 19:11:26 +0300

    some changes to error handlers in auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,30 +1,49 @@
-const { error } = require('utils');
 const express = require('express');
 const passport = require('passport');
 
 const router = express.Router({});
 
-const HttpStatus = require('http-status-codes');
 const User = require('db/models/user');
 
 router.post('/register', (req, res) => {
+  console.log('request: ', req.body);
   const local = passport.authenticate('local');
   const cb = (err) => {
     if (err) {
-      error(res, 'No username was given', HttpStatus.BAD_REQUEST);
-      res.end();
+      res.send({ message: err.message, success: false });
     } else {
-      local(req, res, () => res.redirect('/'));
+      local(req, res, () => res.json({
+        message: `User ${ req.body.username } was successfully registered`,
+        success: true
+      }));
     }
   };
-  User.register(new User({ username: req.body.username }), req.body.password, cb);
+  User.register(
+    new User({ username: req.body.username }),
+    req.body.password,
+    cb,
+  );
 });
 
-router.post('/login', passport.authenticate('local'), (req, res) => res.redirect('/'));
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) { res.send({ message: err.message, success: false }); }
+    if (info) {
+      res.send({ message: info.message, success: false });
+    }
+    if (user) {
+      res.send({ message: `User ${ user.username } successfully authorized`, success: true });
+    }
+  })(req, res, next);
+});
 
-router.get('/logout', (req, res) => {
-  req.logout();
-  res.redirect('/');
+router.get('/logout', (err, req, res) => {
+  if (err) {
+    res.send({ message: err.message, success: false });
+  } else {
+    req.logout();
+    res.send({ message: 'User logged out', success: true });
+  }
 });
 
 module.exports = router;
