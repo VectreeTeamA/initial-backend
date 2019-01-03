aec5bbb3288e60b334f481ef10ffacee0e12d152
Konstantin Alifanov <41705612+Telpenair@users.noreply.github.com>
2019-01-03 22:51:06 +0300

    Issue eslint (#2)
    * eslint prettier init
    
    * airbnb add to eslint
    
    * Update .gitignore

--- db/index.js
+++ db/index.js
@@ -1,3 +1,3 @@
-const User = require("./models/user");
+const User = require('./models/user');
 
-module.exports = {User};
+module.exports = { User };
--- utils/mongoose.js
+++ utils/mongoose.js
@@ -1,11 +1,13 @@
-const mongoose = require("mongoose");
-const config = require("config");
-const log = require("./log")(module);
+const mongoose = require('mongoose');
+const config = require('config');
+const log = require('./log')(module);
 
-mongoose.connect(
-	config.get("database.uri"),
-	config.get("database.options")
-).then(() => log.info("connection with mongo establishment"));
+mongoose
+  .connect(
+    config.get('database.uri'),
+    config.get('database.options'),
+  )
+  .then(() => log.info('connection with mongo establishment'));
 
 mongoose.Promise = global.Promise;
 
